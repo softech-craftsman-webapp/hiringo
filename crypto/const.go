110fbae987d33e6e6496fe98a8c675071f3570bd
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-10-18 18:00:45 +0200

    Crypto algo changed to ECDSA

--- crypto/const.go
+++ crypto/const.go
@@ -7,15 +7,15 @@ import "github.com/golang-jwt/jwt"
    | RSA
    |--------------------------------------------------------------------------
 */
-var SigningMethodName = jwt.SigningMethodRS512.Name
-var SigningMethod = jwt.SigningMethodRS512
-var AccessPublicKey = RsaAccessPublicKey
+// var SigningMethodName = jwt.SigningMethodRS512.Name
+// var SigningMethod = jwt.SigningMethodRS512
+// var AccessPublicKey = RsaAccessPublicKey
 
 /*
    |--------------------------------------------------------------------------
    | ECDSA
    |--------------------------------------------------------------------------
 */
-// var SigningMethodName = jwt.SigningMethodES512.Name
-// var SigningMethod = jwt.SigningMethodES512
-// var AccessPublicKey = EcdsaAccessPublicKey
+var SigningMethodName = jwt.SigningMethodES256.Name
+var SigningMethod = jwt.SigningMethodES256
+var AccessPublicKey = EcdsaAccessPublicKey
