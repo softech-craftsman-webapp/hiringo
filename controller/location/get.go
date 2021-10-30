c856af860c9886cdb1213eb14657c107c20ded79
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-10-30 21:38:31 +0200

    Reverse geocode changed

--- controller/location/get.go
+++ controller/location/get.go
@@ -3,15 +3,54 @@ package location
 import (
 	config "hiringo/config"
 	view "hiringo/view"
+
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
-	"github.com/codingsince1985/geo-golang/openstreetmap"
 	"github.com/labstack/echo/v4"
 )
 
 type CreateLocationRequest struct {
 	Longitude float64 `json:"longitude" validate:"required"`
 	Latitude  float64 `json:"latitude" validate:"required"`
+}
+
+func reverseGeoCode(lat float64, lng float64) (view.LocationView, error) {
+	client := &http.Client{Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}}
+
+	address := view.LocationView{}
+	url := fmt.Sprintf("https://nominatim.openstreetmap.org/reverse?format=json&lat=%f&lon=%f&zoom=18&addressdetails=1", lat, lng)
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		return address, err
+	}
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Accept-Language", "en")
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return address, err
+	}
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return address, err
+	}
+
+	json.Unmarshal(bodyBytes, &address)
+	return address, nil
 }
 
 /*
@@ -47,8 +86,7 @@ func GetLocation(ctx echo.Context) error {
 		})
 	}
 
-	geocoder := openstreetmap.Geocoder()
-	address, error := geocoder.ReverseGeocode(req.Latitude, req.Longitude)
+	address, error := reverseGeoCode(req.Latitude, req.Longitude)
 
 	if error != nil {
 		return view.ApiView(http.StatusBadRequest, ctx, &view.Response{
@@ -62,20 +100,14 @@ func GetLocation(ctx echo.Context) error {
 		Success: true,
 		Message: "Success",
 		Payload: &view.LocationView{
-			Longitude:     req.Longitude,
-			Latitude:      req.Latitude,
-			Address:       address.FormattedAddress,
-			Street:        address.Street,
-			HouseNumber:   address.HouseNumber,
-			Suburb:        address.Suburb,
-			Postcode:      address.Postcode,
-			State:         address.State,
-			StateCode:     address.StateCode,
-			StateDistrict: address.StateDistrict,
-			County:        address.County,
-			Country:       address.Country,
-			CountryCode:   address.CountryCode,
-			City:          address.City,
+			PlaceID:     address.PlaceID,
+			OsmType:     address.OsmType,
+			OsmID:       address.OsmID,
+			Lat:         address.Lat,
+			Lon:         address.Lon,
+			DisplayName: address.DisplayName,
+			Address:     address.Address,
+			Boundingbox: address.Boundingbox,
 		},
 	}
 
--- view/location.go
+++ view/location.go
@@ -1,18 +1,21 @@
 package view
 
 type LocationView struct {
-	Address       string  `json:"address"`
-	Street        string  `json:"street"`
-	HouseNumber   string  `json:"house_number"`
-	Suburb        string  `json:"suburb"`
-	Postcode      string  `json:"postcode"`
-	State         string  `json:"state"`
-	StateCode     string  `json:"state_code"`
-	StateDistrict string  `json:"state_district"`
-	County        string  `json:"county"`
-	Country       string  `json:"country"`
-	CountryCode   string  `json:"country_code"`
-	City          string  `json:"city"`
-	Latitude      float64 `json:"latitude"`
-	Longitude     float64 `json:"longitude"`
+	PlaceID     int    `json:"place_id"`
+	OsmType     string `json:"osm_type"`
+	OsmID       int    `json:"osm_id"`
+	Lat         string `json:"lat"`
+	Lon         string `json:"lon"`
+	DisplayName string `json:"display_name"`
+	Address     struct {
+		HouseNumber  string `json:"house_number"`
+		Road         string `json:"road"`
+		Suburb       string `json:"suburb"`
+		CityDistrict string `json:"city_district"`
+		Region       string `json:"region"`
+		Postcode     string `json:"postcode"`
+		Country      string `json:"country"`
+		CountryCode  string `json:"country_code"`
+	} `json:"address"`
+	Boundingbox []string `json:"boundingbox"`
 }
