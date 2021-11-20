903e41c9658adcdefcade071db1617aaeb693ee9
Emin Muhammadi <muemin17631@sabah.edu.az>
2021-11-20 18:53:12 +0100

    Job permissions updated

--- model/category.go
+++ model/category.go
@@ -11,7 +11,7 @@ type Category struct {
 
 	ID          string         `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"id"`
 	CreatedByID string         `gorm:"type:uuid" json:"created_by"`
-	Name        string         `gorm:"index;type:varchar(64);not null" json:"name"`
+	Name        string         `gorm:"index;type:varchar(64);not null;unique" json:"name"`
 	Description string         `gorm:"null" json:"description"`
 	CreatedAt   time.Time      `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt   time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
